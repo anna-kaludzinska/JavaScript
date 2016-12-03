0090b953119dbd11a361c8d6d92a55c55ca38971
Anna Kaludzinska <anna.kaludzinska@gmail.com>
2016-12-03 10:41:20 +0100

    operators and functions1

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function multiply (parameter1, parameter2) {
+	var sum = parameter1 * parameter2;
+	console.log("Wynikiem mnożenia jest " + sum + ".");
+}
+
+multiply(54,33);
---
+++ js/operators.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var liczba1 = 6;
+var liczba2 = 5;
+
+var wynik;
+
+/*
+wynik = liczba1 * liczba2;
+console.log(wynik);
+*/
+
+/*
+while (liczba2 < liczba1) {
+	console.log(liczba2);
+	liczba2++;
+}
+*/
+
+/*
+(2>5) ? console.log("tak") : console.log("nie");
+*/
+
+/*
+if (("2" !== 2) && (8>4)) {
+	document.write("<br> prawda");
+}
+
+if ( !( ("2" !== 2) && (8>4) ) ) {
+	document.write("<br> prawda");
+} else {
+	document.write("<br> nieprawda");
+}
+*/
+
+
+var liczba3 = 6;
+var liczba4 = 7;
+
+/*
+if (liczba4 >= liczba3) {
+	console.log ("tak, większe"); }
+	else {
+		console.log ("nie, mniejsze"); 
+	}
+*/
+
+( (liczba4 >= liczba3) || (liczba4 == liczba3) ) ? console.log("większe") : console.log("równe");
